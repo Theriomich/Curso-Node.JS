889818eb421272d51fbff4c904c3ddfa999ca038
Michelle~ <theriomich@gmail.com>
2024-05-21 19:35:04 -0400

    Creando primera funcion prueba

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("curso de node.js")
+// var dias = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"]
+// console.log(dias[0])
+
+// let contacto= {"nombre":"Julieta","email":"Julian@gmail.com","Edad":21}
+// console.log(contacto.Edad)
+
+
+// var colors = require("colors")
+// let contacto= {"nombre":"Julieta","email":"Julian@gmail.com","Edad":21}
+// console.log(contacto.email.yellow)
+
+
+var prueba= require ("./Funciones/funcion1");
+prueba.resta(5,2)
